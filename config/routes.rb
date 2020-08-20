577b01f1f331174c27564558e36b8aa6abc161c9
Dan Sehl <dan.sehl@gmail.com>
2020-08-20 13:42:50 -0600

    Add dashboard show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
 
   # Routes for Google Authentication
-  get '/auth/google_oauth2', to: 'sessions#create'
   get 'auth/:provider/callback', to: 'sessions#googleAuth'
   get 'auth/failure', to: redirect('/')
+  get '/dashboard', to: 'dashboard#show'
 end
