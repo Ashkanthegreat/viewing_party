9ec0f3be86b1d381292e76dfbe18cb5b37131221
Dan Sehl <dan.sehl@gmail.com>
2020-08-23 18:51:07 -0600

    Add #top_10 action for MoviesController

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,11 @@
+require_relative '../services/moviedb_service.rb'
+require_relative '../poros/movie.rb'
+require_relative '../facades/movie_search_facade.rb'
+
+
 class MoviesController < ApplicationController
-  def index
-    
+  def top_10
+    movie = MovieSearchFacade.new
+    @movies = movie.top_10
   end
 end
