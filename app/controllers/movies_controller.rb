1d05632a57e7ae9f2a737b620a82099bff817914
Dan Sehl <dan.sehl@gmail.com>
2020-08-24 16:01:23 -0600

    Clean up movies controller to look for top 40 and not top 10 movies
    Co-Authored-By: Ashkan Abbasi <Ashkanthegreat@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,11 @@
-require_relative '../services/moviedb_service.rb'
+require_relative '../services/movie_db_service.rb'
 require_relative '../poros/movie.rb'
 require_relative '../facades/movie_search_facade.rb'
 
 
 class MoviesController < ApplicationController
-  def top_10
+  def top_40
     movie = MovieSearchFacade.new
-    @movies = movie.top_10
+    @movies = movie.top_40
   end
 end
