d0ed63d89b800ecadf9bed463b8c4e7ba002632a
Dan Sehl <dan.sehl@gmail.com>
2020-08-23 18:50:39 -0600

    Add movie DB service, associated connection methods

---
+++ app/services/moviedb_service.rb
@@ -0,0 +1,14 @@
+class MovieDBService
+  def top_rated_movies
+    results = conn.get('movie/top_rated')
+    JSON.parse(results.body, symbolize_names: true)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://api.themoviedb.org/3') do |f|
+      f.params['api_key'] = ENV['MOVIES_API_KEY']
+    end
+  end
+end
