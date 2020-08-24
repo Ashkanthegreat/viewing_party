aa14a6684e6bfcfcc499a3bca4d7e24783586e97
Dan Sehl <dan.sehl@gmail.com>
2020-08-24 16:01:05 -0600

    Rename method top_10 to top_40 in movie search facade
    Co-Authored-By: Ashkan Abbasi <Ashkanthegreat@users.noreply.github.com>

--- app/facades/movie_search_facade.rb
+++ app/facades/movie_search_facade.rb
@@ -3,7 +3,14 @@ class MovieSearchFacade
     @movie_service = MovieDBService.new
   end
 
-  def top_10
-    @movie_service.top_rated_movies
+  def top_40
+    movies_results = @movie_service.top_rated_movies
+    @movies = []
+    while @movies.size < 40
+      movies_results[:results].each do |movie_info|
+        @movies << Movie.new(movie_info)
+      end
+    end
+    @movies
   end
 end
