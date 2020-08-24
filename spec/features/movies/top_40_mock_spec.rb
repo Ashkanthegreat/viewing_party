5b5a460b5fcf85e6d653d7346fb9f11f01ddb533
Ashkan Abbasi <airashkan@yahoo.com>
2020-08-24 17:39:01 -0600

    Test that it can find top 40 rated movies.
    Co-Authored-By: Dan Sehl <dtsehl@users.noreply.github.com>

--- spec/features/movies/top_40_mock_spec.rb
+++ spec/features/movies/top_40_mock_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe "gets the top 40 rated movies from MoviesDB", :vcr do
-  it "allows the user to find the top 40 rated movies" do
-    mock_user
-    visit '/'
-    click_link "Sign in with Google"
-    click_button "Discover Top 40 Rated Movies"
-    expect(page).to have_content("Gabriel")
-    save_and_open_page
+RSpec.describe "gets the top 40 rated movies from MoviesDB" do
+  VCR.use_cassette('returns_the_top_rated_movies') do
+    before :each do
+      @user = mock_user
+      visit '/'
+      click_link "Sign in with Google"
+    end
+    it "allows the user to find the top 40 rated movies" do
+      click_button "Discover Top 40 Rated Movies"
+      expect(current_path).to eq(top_40_path)
+      expect(page).to have_content("Top 40 Rated Movies:")
+      results = MovieSearchFacade.new.top_40
+      expect(page).to have_content(results[0].title)
+    end
   end
 end
