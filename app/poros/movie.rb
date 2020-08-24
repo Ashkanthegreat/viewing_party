82ceb987a7d4bc7a0007ea05f0d6c829e6c0bded
Dan Sehl <dan.sehl@gmail.com>
2020-08-24 16:00:47 -0600

    Remove top_10 method from movie PORO
    Co-Authored-By: Ashkan Abbasi <Ashkanthegreat@users.noreply.github.com>

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,10 +1,6 @@
 class Movie
   attr_reader :id, :title, :vote_average
 
-  def self.top_10
-    @service = MovieDBService.new
-  end
-
   def initialize(movie_info)
     @id = movie_info[:id]
     @title = movie_info[:title]
