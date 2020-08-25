e74800adfa64ebc9a8f6c832776ff73f8c928060
Ashkan Abbasi <airashkan@yahoo.com>
2020-08-25 16:29:38 -0600

    Write spec for searching a movie via keyword.

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe "gets the top 40 rated movies from MoviesDB" do
+RSpec.describe "Movies index page" do
   VCR.use_cassette('returns_the_top_rated_movies') do
     before :each do
       @user = mock_user
@@ -14,6 +14,16 @@ RSpec.describe "gets the top 40 rated movies from MoviesDB" do
       expect(page).to have_content("Top 40 Rated Movies:")
       results = MovieSearchFacade.new.index
       expect(page).to have_content(results[0].title)
+    end
+  end
+  describe 'movie search results' do
+    it 'allows us to discover movies by keyword', :vcr do
+      within ".Movies" do
+        fill_in 'keyword', with: 'Terminator'
+        click_on 'Discover Movies with keyword'
+      end
+      expect(current_path).to eq(index_path)
+      expect(page).to have_content('Terminator')
     end
   end
 end
