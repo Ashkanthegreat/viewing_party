cf00961fc69928e58f0a40e328771900573dc160
Dan Sehl <dan.sehl@gmail.com>
2020-08-26 17:38:22 -0600

    Add movie feature show spec
    Co-Authored-By: Ashkan Abbasi <Ashkanthegreat@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe "Movies index page" do
     click_link "Sign in with Google"
     click_button "Discover Top 40 Rated Movies"
   end
+
   VCR.use_cassette('returns_the_top_rated_movies') do
     it "routes to the movie show page properly" do
       results = MovieSearchFacade.new.top_40
-
       click_link results[0].title
-      expect(current_path).to eq(movies_show_path)
+      expect(current_path).to eq("/movies/#{results[0].id}")
     end
   end
 end
