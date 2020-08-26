b621db7fc1109c7e1e7e0451ec46eceda6456697
Dan Sehl <dan.sehl@gmail.com>
2020-08-26 17:37:31 -0600

    Add return movie details test to movie_db_service_spec.rb

--- spec/services/moviesdb_service_spec.rb
+++ spec/services/moviesdb_service_spec.rb
@@ -21,4 +21,9 @@ RSpec.describe "Movie DB Service", :vcr do
     expect(movies.count).to eq(40)
     expect(movies[0][:original_title]).to_not eq(movies[20][:original_title])
   end
+  it "Returns the details about one movie" do
+    id = 12
+    movie = MovieDBService.new.movie_details(id)
+    expect(movie[:original_title]).to eq("Finding Nemo")
+  end
 end
